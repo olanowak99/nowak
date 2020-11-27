53c3cf562bfff6438ad1095f389a6aac8de8a267
Darek Strojecki <dstroj@wp.pl>
2020-11-27 09:59:12 +0100

    Add context manager to main.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from point import Point
+from point import normalize_angle
 
 
 def main():
-    point_A = Point('A', 1, 2)
-    print(point_A)
+    with open('teksty.txt', 'r') as file:
+        data = file.readlines()
+
+    print(data)
+    print(type(data))
+
+    for line in data:
+        print(line.strip().upper().split())
 
 
 if __name__ == '__main__':
