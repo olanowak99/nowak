1d764f8ab44694c40853f1430c5a75a9b092ea13
Darek Strojecki <dstroj@wp.pl>
2020-12-18 09:27:29 +0100

    Read data from file and make points.

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ def main():
     with open('teksty.txt', 'r') as file:
         data = file.readlines()
 
-    print(data)
-    print(type(data))
+    points = {}
 
-    for line in data:
-        print(line.strip().upper().split())
+    for i in data:
+        i = i.strip().split()
+        nr, x, y = i
+
+        point = Point(nr, x, y)
+        points[nr] = point
+
+    print(points['P1'].get_length(points['P2']))
+    print(points['P2'].get_length(points['P3']))
+    print(points['P3'].get_length(points['P4']))
+
+    # floatA = 1.125
+    # print(round(floatA, 2))
 
 
 if __name__ == '__main__':
