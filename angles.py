7f4741784e4d98cff77ca6bb77fb3128c10a00b0
olanowak99 <oleksaaandra@gmail.com>
2021-01-28 13:09:40 +0100

    zadanie

--- angles.py
+++ angles.py
@@ -1,73 +1,45 @@
 # -*- coding: utf-8 -*-
 import math
-
-
 def deg2grad(deg):
     """
     deg2grad(90.00) --> 100.0
     """
-
-    return None
-
-
+    return (deg * 10) / 9
 def grad2deg(grad):
     """
     grad2deg(100.00) --> 90.0
     """
-
-    return None
-
-
+    return (grad * 9) / 10
 def grad2rad(grad):
     """
     grad2rad(100.0) --> 1.5707963267948968
     """
-
-    return None
-
-
+    return grad * (math.pi / 200)
 def rad2grad(rad):
     """
     rad2grad(1.5707963267948968) --> 90.0
     """
-
-    return None
-
-
+    return rad * (200 / math.pi)
 # ======================== for 3
-
-
 def decimal_deg2rad(decimal_deg):
     """
     decimal_deg(1.5707963267948968) --> 90.0
     """
-
-    return None
-
-
+    return decimal_deg * (math.pi / 180)
 def rad2decimal_deg(rad):
     """
     rad2decimal_deg(1.5707963267948968) --> 90.0
     """
-
-    return None
-
-
+    return rad * (180 / math.pi)
 # ======================== for 4
-
 def decimal_deg2dms_deg(decimal_deg):
     """
     decimal_deg2dms_deg(1.0169722222222222) --> (1, 1, 1.1)
     """
-
     return None
-
-
 def dms_deg2decimal_deg(dms_deg):
     """
     dms_deg2decimal_deg(1, 1, 1.1) --> (1.0169722222222222)
     """
-
     return None
-
 # ======================== for 5
